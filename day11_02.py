c5714c7ba6f775397bea5edf5b478b56328fc6fd
Scott Halgrim <shalgrim@gmail.com>
2020-01-08 09:19:43 -0800

    built out Hex class
    but _assign_neighbors errors out

--- day11_02.py
+++ day11_02.py
@@ -10,25 +10,37 @@ class Direction(Enum):
     NW = auto()
 
     def __add__(self, other):
-        if not isinstance(other, int):
-            raise TypeError(
-                f"'+' not supporrted between instances of 'Direction' and '{type(other)}'"
-            )
-        raise NotImplementedError("haven't done this yet")
+        if isinstance(other, int):
+            newval = self.value + other
+            return Direction(newval % 6)
+        raise TypeError(
+            f"'+' not supported between instances of 'Direction' and '{type(other)}'"
+        )
+
+    def __radd__(self, other):
+        return self.__add__(other)
 
 
 class Hex(object):
     def __init__(self, from_direction=None, from_hex=None):
+        for d in Direction:
+            self.__setattr__(d.name, None)
+
         if from_direction:
             self.__setattr__(from_direction.name, from_hex)
+
         self._assign_neighbors()
-        self.distance = min([n.radius for n in self.get_known_neighbors()]) + 1
+        if not self.get_known_neighbors():
+            self.distance = 0
+        else:
+            self.distance = min([n.radius for n in self.get_known_neighbors()]) + 1
 
     def get_known_neighbors(self):
         known_neighbors = {}
         for d in Direction:
-            if self.__getattribute__(d.name):
-                known_neighbors[d] = self.__getattribute__(d.name)
+            neighbor_hex = self.__getattribute__(d.name)
+            if neighbor_hex:
+                known_neighbors[d] = neighbor_hex
 
         return known_neighbors
 
@@ -37,6 +49,22 @@ class Hex(object):
         known_neighbors = self.get_known_neighbors()
 
         while known_neighbors != old_known_neighbors:
-            new_known_neighbors = {
-                k: v for k, v in known_neighbors.items() if k not in old_known_neighbors
-            }
+            for direction, neighbor in known_neighbors:
+                if not self.__getattribute__(
+                    (direction + 1).name
+                ):  # If I don't already know the clockwise hex
+                    # check if my known neighbor knows about it
+                    clockwise = neighbor.__getattribute__((direction + 2).name)
+                    if clockwise:
+                        self.__setattr__((direction + 1).name, clockwise)
+
+                if not self.__getattribute__(
+                    (direction - 1).name
+                ):  # If I don't already know the ccw hex
+                    # check if my known neighbor knows about it
+                    ccw = neighbor.__getattribute__((direction - 2).name)
+                    if ccw:
+                        self.__setattr__((direction - 1).name, ccw)
+
+            old_known_neighbors = known_neighbors
+            known_neighbors = self.get_known_neighbors()
