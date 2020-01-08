0b1f67a804f4ada7adf17d6472a09c1e2684c091
Scott Halgrim <shalgrim@gmail.com>
2020-01-08 09:32:43 -0800

    fixed some errors
    next is writing tests for Hex and addition/subtraction of Directions

--- day11_02.py
+++ day11_02.py
@@ -10,15 +10,23 @@ class Direction(Enum):
     NW = auto()
 
     def __add__(self, other):
+        """should write some tests"""
         if isinstance(other, int):
-            newval = self.value + other
-            return Direction(newval % 6)
+            zero_indexed = self.value - 1 + other
+            zi_mod = zero_indexed % 6
+            return Direction(zi_mod + 1)
         raise TypeError(
             f"'+' not supported between instances of 'Direction' and '{type(other)}'"
         )
 
     def __radd__(self, other):
         return self.__add__(other)
+
+    def __sub__(self, other):
+        return self.__add__(-other)
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
 
 
 class Hex(object):
@@ -28,12 +36,13 @@ class Hex(object):
 
         if from_direction:
             self.__setattr__(from_direction.name, from_hex)
+            from_hex.__setattr__((from_direction + 3).name, self)
 
         self._assign_neighbors()
         if not self.get_known_neighbors():
             self.distance = 0
         else:
-            self.distance = min([n.radius for n in self.get_known_neighbors()]) + 1
+            self.distance = min([n.distance for n in self.get_known_neighbors().values()]) + 1
 
     def get_known_neighbors(self):
         known_neighbors = {}
@@ -49,7 +58,7 @@ class Hex(object):
         known_neighbors = self.get_known_neighbors()
 
         while known_neighbors != old_known_neighbors:
-            for direction, neighbor in known_neighbors:
+            for direction, neighbor in known_neighbors.items():
                 if not self.__getattribute__(
                     (direction + 1).name
                 ):  # If I don't already know the clockwise hex
@@ -68,3 +77,11 @@ class Hex(object):
 
             old_known_neighbors = known_neighbors
             known_neighbors = self.get_known_neighbors()
+
+
+if __name__ == '__main__':
+    h = Hex()
+    h2 = Hex(Direction.S, h)
+    print(h.get_known_neighbors(), h.distance)
+    print(h2.get_known_neighbors(), h2.distance)
+
