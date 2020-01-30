6ef4480db7a1136041a40df34c7590039a0260e7
Scott Halgrim <shalgrim@gmail.com>
2020-01-29 21:24:56 -0800

    day 14 part 1 success

--- day14_1.py
+++ day14_1.py
@@ -2,7 +2,12 @@ from day10_2 import main as knot_hash
 
 
 def bitify(hash_string):
-    raise NotImplementedError
+    intval = int(hash_string, 16)
+    as_binary = bin(intval)
+    binary_part = as_binary[2:]
+    missing_zeros = 128 - len(binary_part)
+    answer = '0' * missing_zeros + binary_part
+    return answer
 
 
 def make_grid(instring):
@@ -16,12 +21,20 @@ def make_grid(instring):
     return grid
 
 
-def get_free_squares(instring):
+def get_used_squares(instring):
     grid = make_grid(instring)
-    free_squares = 0
+    return get_kind_squares(grid, 1)
+
+
+def get_kind_squares(grid, square_type):
+    used_squares = 0
     for row in grid:
-        ints = [int(c) for c in row]
-        free_squares += 128 - sum(ints)
+        used_squares += sum([int(c) for c in row])
+    if square_type == 1:
+        return used_squares
+    else:
+        return len(grid) * len(grid[0]) - used_squares
 
-    return free_squares
 
+if __name__ == '__main__':
+    print(get_used_squares('ffayrhll'))
--- test_day14.py
+++ test_day14.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
-from day14_1 import get_free_squares
+
+from day14_1 import get_used_squares
 
 
 class TestDay14(TestCase):
     def test_part_1(self):
-        self.assertEqual(get_free_squares('flqrgnkx'), 8108)
+        self.assertEqual(get_used_squares('flqrgnkx'), 8108)
