1119cf9834dcffe21688efb0f30e7a46fff4070d
Scott Halgrim <shalgrim@gmail.com>
2020-01-30 18:29:38 -0800

    adding tests

--- day15_1.py
+++ day15_1.py
@@ -1,8 +1,6 @@
 DIVISOR = 2147483647
 FACTOR_A = 16807
 FACTOR_B = 48271
-INITIAL_A = 65
-INITIAL_B = 8921
 
 
 class Generator(object):
@@ -15,12 +13,12 @@ class Generator(object):
         return self.value
 
 
-def main():
+def main(factor_a, initial_a, factor_b, initial_b, num_iterations):
     matches = 0
-    generator_a = Generator(FACTOR_A, INITIAL_A)
-    generator_b = Generator(FACTOR_B, INITIAL_B)
+    generator_a = Generator(factor_a, initial_a)
+    generator_b = Generator(factor_b, initial_b)
 
-    for i in range(40_000_000):
+    for i in range(num_iterations):
         if generator_a.update_value() % 256 == generator_b.update_value() % 256:
             matches += 1
 
@@ -28,4 +26,7 @@ def main():
 
 
 if __name__ == '__main__':
-    print(main())  # 156059 is too high
+    INITIAL_A = 516
+    INITIAL_B = 190
+    # 156059 is too high, which is what i get with test data...with real input I get even higher 156946
+    print(main(FACTOR_A, INITIAL_A, FACTOR_B, INITIAL_B, 40_000_000))
---
+++ test_day15.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from day15_1 import main, FACTOR_A, FACTOR_B
+
+
+class TestDay15(TestCase):
+    def test_main(self):
+        self.assertEqual(main(FACTOR_A, 65, FACTOR_B, 8921, 2), 0)
+        self.assertEqual(main(FACTOR_A, 65, FACTOR_B, 8921, 3), 1)
+        self.assertEqual(main(FACTOR_A, 65, FACTOR_B, 8921, 5), 1)
+
+    def test_main_long_running(self):
+        # currently returning 156059
+        self.assertEqual(main(FACTOR_A, 65, FACTOR_B, 8921, 40_000_000), 588)
