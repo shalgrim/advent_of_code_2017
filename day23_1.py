db810dfb601a17b46e356634ef8b2a8890fc547a
Scott Halgrim <shalgrim@gmail.com>
2020-04-23 00:24:10 -0700

    day 23 part 1 success

--- day23_1.py
+++ day23_1.py
@@ -2,12 +2,22 @@ from collections import defaultdict
 
 
 class Runner(object):
+<<<<<<< HEAD
     def __init__(self, instructions):
+=======
+    def __init__(self, instructions, debug=False):
+>>>>>>> day 23 part 1 success
         self.instructions = instructions
         self.iptr = 0
         self.registers = defaultdict(lambda: 0)
         self.num_muls = 0
 
+<<<<<<< HEAD
+=======
+        if not debug:
+            self.registers['a'] = 1
+
+>>>>>>> day 23 part 1 success
     def run_instruction(self):
         instruction = self.instructions[self.iptr]
         cmd, x, y = instruction
@@ -43,11 +53,26 @@ def clean_instructions(instructions):
     return instructions
 
 
+<<<<<<< HEAD
 if __name__ == '__main__':
+=======
+def get_day_23_instructions():
+>>>>>>> day 23 part 1 success
     with open('data/input23.txt') as f:
         lines = [line.strip() for line in f.readlines()]
     instructions = [line.split() for line in lines]
     instructions = clean_instructions(instructions)
+<<<<<<< HEAD
     runner = Runner(instructions)
     runner.run()
     print(runner.num_muls)  # 0 is incorrect, unsurprisingly
+=======
+    return instructions
+
+
+if __name__ == '__main__':
+    instructions = get_day_23_instructions()
+    runner = Runner(instructions, debug=True)
+    runner.run()
+    print(runner.num_muls)
+>>>>>>> day 23 part 1 success
